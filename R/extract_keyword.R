5f33915af890865c786ff26e31f857375aaaa706
Steve Kambouris <s.kambouris@student.unimelb.edu.au>
2019-01-23 18:14:24 +1100

    Updates documentation and adds a warning
    The function extract_keyword now shows a warning when the pdf_text function fails for any reason.

--- R/extract_keyword.R
+++ R/extract_keyword.R
@@ -6,25 +6,39 @@
 #' containing the keyword in its immediate context, the page number of the PDF
 #' on which the keyword was found, and the file name of the PDF.
 #'
+#' \code{extract_keyword} relies on the function \code{pdf_text} from the
+#' package \code{pdftools} to extract the the text from each PDF and the
+#' function \code{str_extract_all} from the package \code{stringr} to extract
+#' the keywords from the PDF text; the rest of the function arranges the output
+#' into a neat format.
+#'
 #' @param file A character vector of the PDF file names to search.
 #' @param keyword The keyword to look for in the PDF(s).
-#' @param nbefore The number of words to include before the keyword. The
-#'   default is 0.
-#' @param nafter The number of words to include after the keyword. The default
-#'   is 0.
+#' @param nbefore The number of words to include before the keyword, can be an
+#' integer zero or greater. The default is 0.
+#' @param nafter The number of words to include after the keyword, can be an
+#' integer zero or greater. The default is 0.
 #'
 #' @return A data frame with three variables: \code{PDF_file}, \code{PDF_page},
-#'   and \code{Keyword_extract}.
+#'   and \code{Keyword_extract}. The number of rows in the data frame will
+#'   correspond to the number of times that the keyword was found in the PDF(s)
+#'   searched. If the keyword was not found in any PDFs, there will be no rows
+#'   in the data frame. If the text extraction function fails for all PDFs
+#'   specified in the \code{file} argument, NULL will be returned, along with a
+#'   warning for each of the affected PDF files. If text can be extracted from
+#'   at least one PDF file, the data frame will be returned, but the warnings
+#'   for the affected files will still be shown.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' extract_keyword("example.pdf", "foo")
+#' extract_keyword(c("a.pdf", "b.pdf"), "foo")
 #' }
 extract_keyword <- function(file, keyword, nbefore = 0L, nafter = 0L) {
 
   # Check that none of the essential arguments are missing.
-  stopifnot(!missing(file), !missing(keyword))
+  stopifnot(!missing(file), !missing(keyword), nbefore >= 0, nafter >= 0)
 
   # Create a regular expression based on the keyword and number of words to
   # look before and after the keyword.
@@ -64,6 +78,9 @@ extract_keyword <- function(file, keyword, nbefore = 0L, nafter = 0L) {
       out[[i]] <- format_as_df(keyword_in_context, file[i])
 
     } else {
+      # Show a warning that text could not be extracted.
+      warning(paste0("Text could not be extracted from file ", file[i],". ",
+                     "NULL was returned."))
       # Set the entry corresponding to the current PDF file to null.
       out[[i]] <- NULL
     }
