f8449d41885a70118b5bf7d5fd0a34a113d1fcd3
Steve Kambouris <s.kambouris@student.unimelb.edu.au>
2019-01-23 17:45:46 +1100

    Adds documentation
    Documentation added using Roxygen2.

--- R/extract_keyword.R
+++ R/extract_keyword.R
@@ -1,7 +1,98 @@
-extract_keyword <- function(file, keyword, nbefore = 0L, nbehind = 0L) {
+#' Extract text containing a keyword from a PDF
+#'
+#' \code{extract_keyword} finds all occurrences of a given \code{keyword} in
+#' the text of one or more PDFs, and returns a data frame containing a row for
+#' each occurrence found. Each row of the data frame is made up of a string
+#' containing the keyword in its immediate context, the page number of the PDF
+#' on which the keyword was found, and the file name of the PDF.
+#'
+#' @param file A character vector of the PDF file names to search.
+#' @param keyword The keyword to look for in the PDF(s).
+#' @param nbefore The number of words to include before the keyword. The
+#'   default is 0.
+#' @param nafter The number of words to include after the keyword. The default
+#'   is 0.
+#'
+#' @return A data frame with three variables: \code{PDF_file}, \code{PDF_page},
+#'   and \code{Keyword_extract}.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' extract_keyword("example.pdf", "foo")
+#' }
+extract_keyword <- function(file, keyword, nbefore = 0L, nafter = 0L) {
 
   # Check that none of the essential arguments are missing.
   stopifnot(!missing(file), !missing(keyword))
 
+  # Create a regular expression based on the keyword and number of words to
+  # look before and after the keyword.
+  reg_expr <- paste0("(?i)([^\\s]+\\s){0,", nbefore, "}([^\\s]+){0,1}",
+                     keyword,
+                     "([^\\s]+){0,1}(\\s[^\\s]+){0,", nafter, "}")
 
+  # Create a list to hold the output of each PDF in the file argument.
+  out <- vector(mode = "list", length(file))
+
+  # Create a safe version of the pdf_text function from pdftools.
+  pdf_text_safely <- purrr::safely(pdftools::pdf_text)
+
+  # Loop through the PDFs in the file argument and extract keywords from each.
+  # Return null if the initial text extraction fails.
+
+  # NOTE: This is essentially building in silent failure - the loop will just
+  # move on to the next file after returning null without elaborating on why
+  # the text extraction failed. Consider changing this to show warnings, at
+  # least.
+
+  for (i in seq_along(file)) {
+    # Extract the text from the current PDF, safely.
+    current_pdf_text <- pdf_text_safely(file[i])
+
+    # If the result of the text extraction is not null, try extracting just
+    # the keyword and its context from the text.
+    if (!is.null(current_pdf_text$result)) {
+      # Reduce all extraneous spaces down to single spaces between words.
+      result_reduced <- stringr::str_trim(stringr::str_replace_all(
+        current_pdf_text$result, "[[:space:]]+", " "), side = "both")
+
+      # Extract all keywords in context from the space-reduced text.
+      keyword_in_context <- stringr::str_extract_all(result_reduced, reg_expr)
+
+      # Format the result as a data frame and add to the list.
+      out[[i]] <- format_as_df(keyword_in_context, file[i])
+
+    } else {
+      # Set the entry corresponding to the current PDF file to null.
+      out[[i]] <- NULL
+    }
+  }
+
+  # Compile the list of data frames into a single data frame.
+  out_df <- do.call("rbind", out)
+
+  return(out_df)
+}
+
+format_as_df <- function(inlist, infilename) {
+  # Use melt to turn the list into a data frame, and change the variable
+  # "value" to type "character".
+  out <- reshape2::melt(inlist)
+  out$value <- as.character(out$value)
+
+  # If there are any rows in the data frame, create a variable "PDF_file"
+  # which has the constant value of the passed PDF file name. If the data frame
+  # has no rows, then this should have an empty value.
+  if (dim(out)[1] > 0) {
+    out$PDF_file <- infilename
+  } else {
+    out$PDF_file <- character(0)
+  }
+
+  # Set the names of the other variables in the data frame.
+  names(out)[names(out) == "L1"] <- "PDF_page"
+  names(out)[names(out) == "value"] <- "Keyword_extract"
+
+  return(out)
 }
