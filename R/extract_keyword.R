2b6c3d95e90b4062283bde94b80c0a016ed5388b
Steve Kambouris <s.kambouris@student.unimelb.edu.au>
2019-01-23 16:34:33 +1100

    Adds extract_keyword() code
    Also removes the default hello.R file.

---
+++ R/extract_keyword.R
@@ -0,0 +1,7 @@
+extract_keyword <- function(file, keyword, nbefore = 0L, nbehind = 0L) {
+
+  # Check that none of the essential arguments are missing.
+  stopifnot(!missing(file), !missing(keyword))
+
+
+}
